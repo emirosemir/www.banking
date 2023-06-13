721dff3272cc31e6adab4a83439b3bbca47230d6
Emirhan Demirel <emirhan.demirel@algorichma.com>
2023-06-13 23:46:55 +0300

    Edited the homepage (app.blade.php) top menu.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,34 +23,30 @@
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+        <nav class="navbar navbar-expand-lg bg-body-tertiary" style="background-color: #002952;">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="{{ url('/') }}" style="color: white;">{{ config('app.name', 'Laravel') }}</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
-
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav me-auto">
-
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        
                     </ul>
-
                     <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ms-auto">
                         <!-- Authentication Links -->
                         @guest
                             @if (Route::has('login'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                                <li class="nav-item d-flex align-items-center me-2">
+                                    <a href="{{ route('login') }}" type="button" class="btn btn-outline-warning btn-sm">{{ __('Login') }}</a>
                                 </li>
                             @endif
 
                             @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                <li class="nav-item d-flex align-items-center">
+                                    <a href="{{ route('register') }}" type="button" class="btn btn-outline-warning btn-sm">{{ __('Register') }}</a>
                                 </li>
                             @endif
                         @else
@@ -76,7 +72,6 @@
                 </div>
             </div>
         </nav>
-
         <main class="py-4">
             @yield('content')
         </main>
